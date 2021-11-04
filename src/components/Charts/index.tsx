b8779815bb08a49a59d38e83e1857fec0252198c
quimack <75865290+quimack@users.noreply.github.com>
2021-11-04 00:53:24 -0300

    Add last month bookings fluctuation line chard

--- src/components/Charts/index.tsx
+++ src/components/Charts/index.tsx
@@ -1,50 +1,76 @@
 import moment from 'moment';
-import { FC, useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useQuery } from 'react-query';
 import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
 import { getBookings } from '../../api';
-import { DATE_FORMATS } from '../../contrants/date-formats';
 import { QUERY_KEYS } from '../../contrants/query-keys';
 import { Booking } from '../../types';
 
 
-
-export const LastMonthBookingsChart= () => {
+export const LastMonthBookingsChart = () => {
 
   const { data: allBookings } = useQuery<Booking[]>(QUERY_KEYS.ALL_BOOKINGS, () => getBookings());
 
+  //Gettin first and last date from the last month
   const [lastMonthLastDate, setLastMonthLastDate] = useState(moment().date(0));
   const [lastMonthFirstDate, setLastMonthFirstDate] = useState(moment().date(0).date(1));
-
+  //Gettin the last month bookings
   const lastMonth = allBookings?.filter(booking => moment(booking.check_in_date).isBetween(lastMonthFirstDate, lastMonthLastDate));
-  console.log(lastMonth)
-  
-  const data = [1,2,3,4,5];
+   
+  //Setting data from the chart
+  const [data, setData] = useState([
+    {
+      week: "First week",
+      bookings: 0
+    },
+    {
+      week: "Second week",
+      bookings: 0
+    },
+    {
+      week: "Third week",
+      bookings: 0
+    },
+    {
+      week: "Fourth week",
+      bookings: 0
+    }
+    ])
 
-  const setingData = () => {
+  //Separatting last month bookings in four weeks, for X axis  
+  useEffect(()=>{
+    //Resetting to zero the bookings counter
+    data[0].bookings = 0
+    data[1].bookings = 0
+    data[2].bookings = 0
+    data[3].bookings = 0
+
     lastMonth?.map((booking) =>{
-        if(moment(booking.check_in_date).isBetween(moment().date(0).date(1), moment().date(0).date(1).date(7))){
-          console.log("1er semana")
-        }
-        if(moment(booking.check_in_date).isBetween(moment().date(0).date(1).date(8), moment().date(0).date(1).date(14))){
-          console.log("2dasemana")
-        }
-        if(moment(booking.check_in_date).isBetween(moment().date(0).date(1).date(15), moment().date(0).date(1).date(21))){
-          console.log("3dasemana")
-        }
-        if(moment(booking.check_in_date).isBetween(moment().date(0).date(1).date(22), moment().date(0))){
-          console.log("4dasemana")
-        }
+      if(moment(booking.check_in_date).isBetween(moment().date(0).date(1), moment().date(0).date(1).date(8))){
+        data[0].bookings = data[0].bookings + 1;
+        setData(data);
+      }
+      if(moment(booking.check_in_date).isBetween(moment().date(0).date(1).date(9), moment().date(0).date(1).date(15))){
+        data[1].bookings = data[1].bookings + 1;
+        setData(data);
+      }
+      if(moment(booking.check_in_date).isBetween(moment().date(0).date(1).date(15), moment().date(0).date(1).date(22))){
+        data[2].bookings = data[2].bookings + 1;
+        setData(data);
+      }
+      if(moment(booking.check_in_date).isBetween(moment().date(0).date(1).date(22), moment().date(0))){
+        data[3].bookings = data[3].bookings + 1;
+        setData(data);
+      }
     })
-  }
-  setingData();
+  }, [allBookings ])
 
     return(
     <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
-    <Line type="monotone" dataKey="number_of_guests" stroke="#8884d8" />
+    <Line type="monotone" dataKey="bookings" stroke="#8884d8" />
     <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-    <XAxis dataKey="date" />
-    <YAxis />
+    <XAxis dataKey="week" />
+    <YAxis /> 
   </LineChart>
     )
 };
